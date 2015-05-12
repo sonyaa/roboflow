797ced7f45709704666b0baebc2a62a24a47b071
Sonya Alexandrova <sonyaa@cs.washington.edu>
2015-05-12 15:59:25 -0700

    only load parameters once

--- nodes/action_manager.py
+++ nodes/action_manager.py
@@ -12,8 +12,6 @@ roslib.load_manifest('roboflow')
 
 
 def save_roboflow_action(req):
-    data_directory = rospy.get_param('/roboflow/actionsRoot')
-    file_extension = rospy.get_param('/roboflow/fileExtension', '.json')
     file_name = data_directory + str(req.action_id) + file_extension
     if not os.path.exists(data_directory):
         os.makedirs(data_directory)
@@ -26,8 +24,6 @@ def save_roboflow_action(req):
 
 def get_saved_roboflow_actions(dummy):
     actions = []
-    data_directory = rospy.get_param('/roboflow/actionsRoot')
-    file_extension = rospy.get_param('/roboflow/fileExtension', '.json')
     if not os.path.exists(data_directory):
         return []
     for f in listdir(data_directory):
@@ -35,12 +31,13 @@ def get_saved_roboflow_actions(dummy):
         if isfile(file_path) and file_path.endswith(file_extension):
             with open(file_path, 'r') as content_file:
                 actions.append(content_file.read())
-                rospy.loginfo('Getting action from file ' + file_path)
     rospy.loginfo('Loaded ' + str(len(actions)) + ' file(s).')
     return GetSavedRoboflowActionsResponse(actions)
 
 
 if __name__ == '__main__':
+    data_directory = rospy.get_param('/roboflow/actionsRoot')
+    file_extension = rospy.get_param('/roboflow/fileExtension', '.json')
     rospy.init_node('roboflow_action_manager')
     rospy.Service('get_saved_roboflow_actions', GetSavedRoboflowActions, get_saved_roboflow_actions)
     rospy.Service('save_roboflow_action', SaveRoboflowAction, save_roboflow_action)
